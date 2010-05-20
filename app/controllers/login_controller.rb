94e4204f144420c00e7742971bebad8eb86570c6
Thor Kell <thor@soundcloud.com>
2010-05-20 10:44:15 +0200

    Changing from Oauth1.0 to 1.0a.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -6,25 +6,40 @@ class LoginController < ApplicationController
   # this will get a oauth request token from Soundcloud and 
   # will redirect the user to the Soundcloud authorize page
   # it stores request token and secret in the session to remember it, when he returns to the callback page
-  def redirect  
-    request_token = $sc_consumer.get_request_token
-    session[:request_token] = request_token.token
-    session[:request_token_secret] = request_token.secret
-    callback_url = url_for :action => :callback
-    authorize_url = "http://api.soundcloud.com/oauth/authorize?oauth_token=#{request_token.token}&oauth_callback=#{callback_url}"
+  
+  # Insert your consumer key and secret here:  
+  $sc_consumer = Soundcloud.consumer('YOUR_CONSUMER_KEY','YOUR_CONSUMER_SECRET')
+  
+  def redirect
+    sc_request_token = $sc_consumer.get_request_token()
+    session[:sc_request_token] = sc_request_token.token
+    session[:sc_request_token_secret] = sc_request_token.secret
+    @authorize_url = "http://soundcloud.com/oauth/authorize?oauth_token=#{sc_request_token.token}"
 
-    redirect_to authorize_url
+    #Redirect the user to authorize_url, in a platform correct browser
+    if RUBY_PLATFORM =~ /darwin|linux|mswin|mingw|bccwin|wince|emx/
+        if RUBY_PLATFORM =~ /darwin/
+          puts "here?"
+          `open #{@authorize_url}`
+        elsif RUBY_PLATFORM =~ /linux/ && `which firefox` != ""
+          `firefox #{@authorize_url}`
+        elsif RUBY_PLATFORM =~ /mswin|mingw|bccwin|wince|em/ 
+        	`start #{@authorize_url}`
+        end
+    	else
+        	puts "Please open #{@authorize_url} in a web browser.  "
+    end
   end
 
   # after authentication at the Soundcloud authorize page, the user will land here
   # we get the access_token and use it to get the Soundcloud user resource 
-  # then we look in our db if we know this user already otherwise we add him.
-  # he will be redirected to the loggedin page.
+  # then we look in our db to see if we know this user already otherwise we add him.
+  # he will then be redirected to the loggedin page.
   def callback
-    request_token = OAuth::RequestToken.new($sc_consumer, session[:request_token], session[:request_token_secret])
-    access_token = request_token.get_access_token
-    
-    sc = Soundcloud.register({:access_token => access_token})
+    @oauth_verifier = params[:oauth_verifier]
+    sc_request_token = OAuth::RequestToken.new($sc_consumer, session[:sc_request_token], session[:sc_request_token_secret])
+    sc_access_token = sc_request_token.get_access_token(:oauth_verifier => @oauth_verifier)  
+    sc = Soundcloud.register({:access_token => sc_access_token})
     
     me = sc.User.find_me
 
@@ -32,18 +47,17 @@ class LoginController < ApplicationController
     user = User.find_by_sc_user_id(me.id)
     if user.nil?
       user = User.create({:sc_user_id => me.id, :sc_username => me.username,
-               :access_token => access_token.token, :access_token_secret => access_token.secret })
+               :access_token => sc_access_token.token, :access_token_secret => sc_access_token.secret })
     else
       user.sc_username = me.username
-      user.access_token = access_token.token
-      user.access_token_secret = access_token.secret
+      user.access_token = sc_access_token.token
+      user.access_token_secret = sc_access_token.secret
       user.save!
     end
     
     session[:user_id] = user.id
     redirect_to :controller => :home
   end
-  
   
   # delete his session and redirect to home
   def logout
